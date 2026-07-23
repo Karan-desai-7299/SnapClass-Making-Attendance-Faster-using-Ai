a017fec0844afcbcad24a0d7b2d93d8644f86fd5
Karansinh Desai <karansinhdesai91@gmail.com>
2026-07-23 11:36:19 +0530

    Improve UI responsiveness

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -5,7 +5,7 @@ import io
 
 @st.dialog("Share Class Invitation")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "https://snapclass-main-karansinh.streamlit.app"
+    app_domain = "snapclass-main-karansinh787repo.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.markdown(
